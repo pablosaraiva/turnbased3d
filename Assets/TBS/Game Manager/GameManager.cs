f2c16e69139599c4c7ae313b23d7c95c5749fd76
Pablo Saraiva <pablosaraiva@gmail.com>
2016-02-14 11:58:25 -0200

    Indoor Scene

---
+++ Assets/TBS/Game Manager/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class GameManager : MonoBehaviour {
+
+    public AICharacterControl[] aiCharacters;
+    private AICharacterControl currentPlayer;
+
+    void FixedUpdate()
+    {
+        if (currentPlayer == null || currentPlayer.isPlaying == false)
+        {
+            foreach (AICharacterControl player in aiCharacters)
+            {
+                if (player.currentCooldown == 0)
+                {
+                    currentPlayer = player;
+                    player.beginTurn();
+                    return;
+                }
+            }
+
+            foreach (AICharacterControl player in aiCharacters)
+            {
+                player.currentCooldown--;
+            }
+        }
+    }
+}
